f8a6cef0dd9e4bc38033b44cecf331921cfa468c
Matthew Nowell <matthewnowell97@gmail.com>
2022-07-06 12:08:22 -0400

    UpdatedDao

--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -1,7 +1,17 @@
 package com.techelevator.tenmo.dao;
 
+import com.techelevator.tenmo.model.Account;
+
+import java.util.List;
+
 public interface AccountDao {
 
+    List<Account> getAccounts();
+    List<Account> getAccountsByUserID(int userId);
+    Account individualAccount(int accountId);
+    Account createAccount(Account accountToAdd);
+    void updateAccount(Account accountToUpdate);
+    void deleteAccount(int accountId);
 
 }
 
