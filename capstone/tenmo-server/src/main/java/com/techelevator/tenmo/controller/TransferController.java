1b5d93507d629331f6912d23db5b6b3f449ab0b0
Samuel Muskovitz <samuel.muskovitz@gmail.com>
2022-07-07 14:20:45 -0400

    added authorization

--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
@@ -5,6 +5,7 @@ import com.techelevator.tenmo.dao.TransferDao;
 import com.techelevator.tenmo.model.Account;
 import com.techelevator.tenmo.model.Transfer;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@PreAuthorize("isAuthenticated()")
 public class TransferController {
 
     private TransferDao transferDao;
